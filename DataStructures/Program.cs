66db600dae113e1c36c0ed533567e98e4e3e435e
Saravanakumar Muthusamy (Infosys Ltd) <v-saramu@microsoft.com>
2019-12-05 21:52:21 -0800

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfNth(5));
+            Console.WriteLine(SumOfNthNumber(5));
+            Console.ReadLine();
+        }
+        //Sum of N number, O(N)
+        public static int SumOfNth(int number)
+        {
+            int total = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        //Sum of N number without varriable, O(1)
+        public static int SumOfNthNumber(int number)
+        {
+            return (number * (number + 1) / 2);
+        }
+
+    }
+}
